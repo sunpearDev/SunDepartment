d706d4b2c896d212ebaca04fea2bfb5ff0fe32fd
SunPear <sunpear.sp@gmail.com>
2021-07-12 14:17:08 +0700

    change config host and db

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
 import '../css/HomePage.css'
+const host = 'http://' + window.location.hostname
 
 export default class HomePage extends Component {
     render() {
         return (
-            <div className="landingview" style={{ backgroundImage: "url(http://localhost:3000/images/view.jpg)" }}>
+            <div className="landingview" style={{ backgroundImage: `url(${host}/images/view.jpg)` }}>
                 <div className="h1-slider-caption">
                     <h1 data-animation="fadeInUp" data-delay=".4s" className style={{ animationDelay: '0.4s' }}>sun department</h1>
                     <h3 data-animation="fadeInDown" data-delay=".4s" className style={{ animationDelay: '0.4s' }}>The Best Hotel &amp; Resourt</h3>
--- connection.js
+++ connection.js
@@ -2,11 +2,11 @@ const mysql = require('mysql')
 
 //create connect to msql
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
+    host: 'bgln0iskewmff7ykixfa-mysql.services.clever-cloud.com',
+    user: 'u2ogpsmix7fcivpq',
+    password: 'ZLHJm8Ujnol4fRyW5g1A',
     port: 3306,
-    database: 'sun_department'
+    database: 'bgln0iskewmff7ykixfa'
 })
 
 module.exports = connection
